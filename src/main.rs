16d847872d241e6a330db42291e7deb1c8d62e0f
Leon Zhao <leeeon233@gmail.com>
2025-11-13 17:33:55 +0800

    feat: add CLI configuration parsing (#74)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
+use clap::Parser;
 use codex_arg0::arg0_dispatch_or_else;
 use codex_common::CliConfigOverrides;
 
 fn main() -> Result<()> {
     arg0_dispatch_or_else(|codex_linux_sandbox_exe| async move {
-        codex_acp::run_main(codex_linux_sandbox_exe, CliConfigOverrides::default()).await?;
+        let cli_config_overrides = CliConfigOverrides::parse();
+        codex_acp::run_main(codex_linux_sandbox_exe, cli_config_overrides).await?;
         Ok(())
     })
 }
